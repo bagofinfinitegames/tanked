18c8280c9c80b9bb93d6b42b67e38b3ceeceb251
aneylon <arlen.m.neylon@gmail.com>
2020-03-01 00:03:12 -0700

    Add model and character info.

---
+++ Tanked/Assets/Scripts/Player01.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01 : MonoBehaviour
+{
+    public GameObject CannonPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
